7b55658103a3b3634c4fff7349b29806ff952288
Eliran <eliranmal@gmail.com>
2012-08-19 15:21:38 +0300

    removed redundant code

--- src/com/starbucks/locator/view/ViewManager.java
+++ src/com/starbucks/locator/view/ViewManager.java
@@ -1,15 +1,11 @@
 package com.starbucks.locator.view;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
-
-import com.starbucks.locator.model.dto.Transferable;
-import com.starbucks.locator.util.AppConstants;
 
 @SuppressWarnings("serial")
 public class ViewManager extends HttpServlet {
@@ -18,22 +14,7 @@ public class ViewManager extends HttpServlet {
 	public void service(ServletRequest req, ServletResponse res) throws ServletException,
 			IOException {
 
-		String cmd = req.getParameter(AppConstants.REQ_PARAM_NAME_COMMAND);
-
-		
-		
-		Object ajaxRes = req.getAttribute(AppConstants.REQ_ATTR_NAME_AJAX_RES);
-		Transferable resText = (Transferable) ajaxRes;
-
-		// TODO format resText if necessary
-
-		
-		
-		PrintWriter out;
-		out = res.getWriter();
-		out.write(resText.asString());
-		out.flush();
-
+		// not used - ajax redirects back with no dynamic updates to the document
 	}
 
 }
